4f4fd9e97e4c577966dbbe86dbca569f3e0a8ee0
SANTIAGO MARCELO FIERRO DAVILA <smfierrod@utn.edu.ec>
2022-07-02 21:06:23 -0500

    Agendar Citas Paciente

--- pacientes/urls.py
+++ pacientes/urls.py
@@ -1,7 +1,10 @@
 from django.urls import  path
 from . import views
-
+from django.contrib.auth import views as views_django
 urlpatterns = [
     path('', views.panel_principal, name='panel_pacientes'),
-    path('registro/', views.registro, name='registro_pacientes')
+    path('<especialidad>', views.panel_principal, name='panel_pacientes'),
+    path('registro/', views.registro, name='registro_pacientes'),
+    path('agendar_cita/<id_turno>/<fecha_mostrada>/<especialidad_mostrada>', views.agendar_cita, name='agendar-cita'),
+     path('logout/', views_django.logout_then_login, name='logout-paciente')
 ]
--- pacientes/views.py
+++ pacientes/views.py
@@ -3,12 +3,15 @@ from django.http import HttpResponse
 from verify_email.email_handler import send_verification_email
 from django.core.files.base import ContentFile
 from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseRedirect
 #Modelos
 from .models import Paciente
 from django.contrib.auth.models import User, Group
-
+from medicos.models import Turno, Medico, Especialidad
 #Formularios
 from .forms import UserForm, PacienteForm
+#Python
+from datetime import datetime, timedelta, date
     
 def registro(request):
     if request.method == 'POST':
@@ -36,8 +39,97 @@ def registro(request):
         paciente_form = PacienteForm()
         return render(request, 'pacientes/registro.html', {'user_form': user_form, 'paciente_form':paciente_form})
 
+
+
+#----------------------------------------------Panel Principal--------------------------------
+
 def verifica_paciente(user):
     return user.groups.filter(name='Pacientes').exists()
 
-def panel_principal(request):
+
+def get_turnos_semana(fecha, id_especialidad):
+    inicio_semana = fecha - timedelta(days=fecha.weekday())
+    fin_semana = inicio_semana + timedelta(days=7)
+
+    #Busca todos los turnos en las fechas establecidas y de la especialidad definida, EL truno debe estar libre y no completado
+    turnos = Turno.objects.filter(fecha__gte=inicio_semana, fecha__lt=fin_semana,paciente=None, completado=False, medico__especialidad=id_especialidad).order_by('fecha')
+
+    fechas_agrupadas = {}
+    for i in range(7):
+        fecha_grupo = inicio_semana+timedelta(days=i)
+        fechas_agrupadas[fecha_grupo] = turnos.filter(fecha__gte=fecha_grupo, fecha__lt=fecha_grupo+timedelta(days=1))
+
+    return fechas_agrupadas
+
+@user_passes_test(verifica_paciente)
+def panel_principal(request, especialidad=None):
+
+
+    #Mensaje a deplegarse
+    mensaje = request.GET.get('mensaje')
+
+    if(especialidad is None):
+        #Escoje la primiera especialidad encontrada
+            especialidad = Especialidad.objects.all()[0]
+    else:
+        #Verifica que la especialidad exista
+        try:
+            especialidad = Especialidad.objects.get(nombre=especialidad)
+        except Especialidad.DoesNotExist:
+            especialidad = Especialidad.objects.all()[0]
+
+
+    #Recibe la fecha a mostrarse
+    fecha = request.GET.get('fecha')
+    if(fecha is None):
+        fecha = date.today()
+    else:
+        fecha = datetime.strptime(fecha, '%d-%m-%Y')
+    #Recibe el id de la especialidad a mostrarse
+   
+
+    turnos = get_turnos_semana(fecha, especialidad.id)
+
+    #Guarda el nombre completo de cada Medico de un turno
+    medicos = {}
+    for turnos_dia in turnos:
+        turnos_dia = turnos[turnos_dia]
+        for turno in turnos_dia:
+            medico = Medico.objects.get(id=turno.medico.id)
+            user = User.objects.get(id=medico.user.id)
+            medicos[turno.id] = user.first_name +" "+ user.last_name 
+
+
+    #Calcula las fechas sugiente y anteriror
+    anterior_semana = (fecha - timedelta(days=fecha.weekday())) - timedelta(days=7)
+    siguiente_semana = (fecha - timedelta(days=fecha.weekday())) + timedelta(days=7)
+
+    return render(request, 'pacientes/panel_principal.html', 
+        {'turnos':turnos, 
+        'medicos':medicos, 
+        'siguiente_semana':siguiente_semana,
+        'anterior_semana':anterior_semana,
+        'especialidad_mostrada':especialidad.nombre,
+        'fecha_mostrada': fecha,
+        'especialidades':Especialidad.objects.all(),
+        'mensaje':mensaje
+        })
+
+
     return render(request, 'pacientes/panel_principal.html')
+
+@user_passes_test(verifica_paciente)
+def agendar_cita(request, id_turno, fecha_mostrada, especialidad_mostrada):
+    try:
+        turno = Turno.objects.get(id=id_turno, completado=False, paciente=None)
+        paciente = Paciente.objects.get(user=request.user.id)
+        #Comprueba conflicto de horarios del paciente
+        if(Turno.objects.filter(fecha=turno.fecha, paciente=paciente.id).exists()):
+            return HttpResponseRedirect('/pacientes/'+ especialidad_mostrada+ '?fecha='+fecha_mostrada+'&mensaje=Conflicto de Horarios')
+        turno.paciente = paciente
+        turno.save()
+        return HttpResponseRedirect('/pacientes/'+ especialidad_mostrada+ '?fecha='+fecha_mostrada+'&mensaje=Cita Agendada')
+    except Turno.DoesNotExist:
+        return HttpResponseRedirect('/pacientes/'+ especialidad_mostrada+ '?fecha='+fecha_mostrada+'&mensaje=Error: El turno se ha ocupado')
+
+    
